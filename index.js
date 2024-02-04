594b220ccf0c7493dcfc802d08f4501eeb4ea791
Shan <shansiddiqui94@gmail.com>
2024-02-04 13:44:14 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+function multiply(num1,num2){
+    return num1 * num2
+}
+
+function random(ran){
+    return Math.random(ran)
+}
+
+function mod(num3, num4){
+    return num3 % num4
+}
+
+function max(){
+
+}
